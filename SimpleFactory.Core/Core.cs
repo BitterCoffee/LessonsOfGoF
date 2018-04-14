85821a2bf4409c4d08b78933f40a42ad7889bb3c
甜咖啡qiji <qijin2001@outlook.com>
2018-04-14 22:47:25 +0800

    create

---
+++ SimpleFactory.Core/Core.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory.Core
+{
+    public abstract class Calculate
+    {
+        public double NumA { get; set; } = 0d;
+        public double NumB { get; set; } = 0d;
+        public abstract double GetResult();
+    }
+
+    /// <summary>
+    /// 加法
+    /// </summary>
+    public class CalculateAdd : Calculate
+    {
+        public override double GetResult()
+        {
+            return NumA + NumB;
+        }
+    }
+
+    /// <summary>
+    /// 减法
+    /// </summary>
+    public class CalculateSub : Calculate
+    {
+        public override double GetResult()
+        {
+            return NumA - NumB;
+        }
+    }
+
+    /// <summary>
+    /// 乘法
+    /// </summary>
+    public class CalculateMul : Calculate
+    {
+        public override double GetResult()
+        {
+            return NumA * NumB;
+        }
+    }
+
+    /// <summary>
+    /// 除法
+    /// </summary>
+    public class CalculateDiv : Calculate
+    {
+        public override double GetResult()
+        {
+            if (NumB == 0)
+            {
+                throw new Exception("不能除以零。");
+            }
+            return NumA / NumB;
+        }
+    }
+
+    /// <summary>
+    /// 幂
+    /// </summary>
+    public class CalculatePow : Calculate
+    {
+        public override double GetResult()
+        {
+            return Math.Pow(NumA, NumB);
+        }
+    }
+
+    public class CalculateFactory
+    {
+        public static Calculate CreateCalculate(string operate)
+        {
+            Calculate target = null;
+            switch (operate)
+            {
+                case "+": target = new CalculateAdd(); break;
+                case "-": target = new CalculateSub(); break;
+                case "*": target = new CalculateMul(); break;
+                case "/": target = new CalculateDiv(); break;
+                case "^": target = new CalculatePow(); break;
+            }
+            return target;
+        }
+    }
+}
---
+++ SimpleFactory.Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using SimpleFactory.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operate = "+";
+            var cal = CalculateFactory.CreateCalculate(operate);
+            cal.NumA = 1d;
+            cal.NumB = 2d;
+            Console.WriteLine("Calculate: {0}{1}{2}={3}", cal.NumA, operate, cal.NumB, cal.GetResult());
+
+            operate = "-";
+            cal = CalculateFactory.CreateCalculate(operate);
+            cal.NumA = 3d;
+            cal.NumB = 4d;
+            Console.WriteLine("Calculate: {0}{1}{2}={3}", cal.NumA, operate, cal.NumB, cal.GetResult());
+
+
+            operate = "*";
+            cal = CalculateFactory.CreateCalculate(operate);
+            cal.NumA = 5d;
+            cal.NumB = 6d;
+            Console.WriteLine("Calculate: {0}{1}{2}={3}", cal.NumA, operate, cal.NumB, cal.GetResult());
+            
+            operate = "/";
+            cal = CalculateFactory.CreateCalculate(operate);
+            cal.NumA = 7d;
+            cal.NumB = 8d;
+            Console.WriteLine("Calculate: {0}{1}{2}={3}", cal.NumA, operate, cal.NumB, cal.GetResult());
+            
+            operate = "^";
+            cal = CalculateFactory.CreateCalculate(operate);
+            cal.NumA = 9d;
+            cal.NumB = 10d;
+            Console.WriteLine("Calculate: {0}{1}{2}={3}", cal.NumA, operate, cal.NumB, cal.GetResult());
+
+            Console.ReadLine();
+        }
+    }
+}
